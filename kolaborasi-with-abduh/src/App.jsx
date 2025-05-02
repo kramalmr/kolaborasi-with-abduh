56721350ae77edbdf4e9c314da4728b662dca4af
abduhhtm <abduh2605@student.abudzar.sch.id>
2025-05-02 11:02:47 +0700

    abdul

--- kolaborasi-with-abduh/src/App.jsx
+++ kolaborasi-with-abduh/src/App.jsx
@@ -3,6 +3,8 @@ export default function App() {
     <div>
       <h1 className="text-3xl font-bold text-center my-10">Ini Project Akram</h1>
       <img src="/akram.jpeg" alt="" className="w-1/2 mx-auto"/>
+      <h1 className="text-3xl font-bold text-center my-10">Ini kolaborasi Akram</h1>
+      <img src="/fl.jpg" alt="" className="w-1/2 mx-auto"/>
     </div>
   );
 }
